f7e11f411d473b98f789b18dffe76dbb7433d283
louie <louie@louies-Mac.local>
2016-06-20 19:50:43 -0700

    Add Most Commong Character method

--- Alien Adventure/MostCommonCharacter.swift
+++ Alien Adventure/MostCommonCharacter.swift
@@ -9,6 +9,31 @@
 extension Hero {
     
     func mostCommonCharacter(inventory: [UDItem]) -> Character? {
+        if inventory.count < 1 {
         return nil
+        }
+        
+        var charCounts = [Character:Int]()
+        var maxChar: Character?
+        var maxCount = 0
+        
+        for item in inventory {
+            for chars in item.name.lowercaseString.characters {
+                if charCounts[chars] != nil {
+                    charCounts[chars]! += 1
+                }
+                else {
+                    charCounts.updateValue(1, forKey: chars)
+                }
+            }
+        }
+        
+        for item in charCounts {
+            if item.1 > maxCount {
+                maxCount = item.1
+                maxChar = item.0
+            }
+        }
+        return maxChar
     }
 }
