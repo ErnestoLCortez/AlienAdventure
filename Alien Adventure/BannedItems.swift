fa6a3a87607ae8e6bd421aacd8dd58ebcb3650c9
louie <louie@louies-Mac.local>
2016-06-20 18:58:53 -0700

    Add BannedItems and PlanetData methods

--- Alien Adventure/BannedItems.swift
+++ Alien Adventure/BannedItems.swift
@@ -11,8 +11,22 @@ import Foundation
 extension Hero {
     
     func bannedItems(dataFile: String) -> [Int] {
-        return [Int]()
+        var bannedItems = [Int]()
+        let dataFileURL = NSBundle.mainBundle().URLForResource(dataFile, withExtension: "plist")!
+        let importedList = NSArray(contentsOfURL: dataFileURL) as! [[String:AnyObject]]
+        
+        for item in importedList {
+            if let historicalData = item["HistoricalData"] as? [String:AnyObject]{
+                if let age = historicalData["CarbonAge"] as? Int where age < 30 {
+                    print(age)
+                    if let name = item["Name"] as? String where name.containsString("Laser") || name.containsString("laser"){
+                        print(name)
+                        bannedItems.append(item["ItemID"] as! Int)
+                    }
+                }
+            }
+        }
+        return bannedItems
     }
 }
-
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 6"
--- Alien Adventure/PlanetData.swift
+++ Alien Adventure/PlanetData.swift
@@ -10,7 +10,41 @@ import Foundation
 extension Hero {
     
     func planetData(dataFile: String) -> String {
-        return ""
+        let dataFileURL = NSBundle.mainBundle().URLForResource(dataFile, withExtension: "json")!
+        let rawList = NSData(contentsOfURL: dataFileURL)!
+        
+        var importedList: [[String:AnyObject]]!
+        do {
+            importedList = try! NSJSONSerialization.JSONObjectWithData(rawList, options: NSJSONReadingOptions()) as! [[String:AnyObject]]
+        }
+        
+        var mostIntriguingPlanet = ""
+        var maxScore = -1
+        
+        for planet in importedList {
+            if let name = planet["Name"] as? String {
+                var totalPoints = 0
+            
+                if let commons = planet["CommonItemsDetected"] as? Int {
+                    totalPoints += commons
+                }
+                if let uncommons = planet["UncommonItemsDetected"] as? Int{
+                    totalPoints += uncommons * 2
+                }
+                if let rares = planet["RareItemsDetected"] as? Int {
+                    totalPoints += rares * 3
+                }
+                if let legendaries = planet["LegendaryItemsDetected"] as? Int {
+                    totalPoints += legendaries * 4
+                }
+                if totalPoints > maxScore {
+                    mostIntriguingPlanet = name
+                    maxScore = totalPoints
+                }
+            }
+        }
+        
+        return mostIntriguingPlanet
     }
 }
 
